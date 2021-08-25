a621eaee06ac27916894a54518b93def08f90aee
Evan Coulson <evan.m.coulson@gmail.com>
2021-08-25 10:47:43 -0700

    added file database project

---
+++ FileDatabase/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FileDatabase
+{
+    public class Class1
+    {
+    }
+}
--- Ordering/Repository/OrderFileRepository.cs
+++ Ordering/Repository/OrderFileRepository.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using Ordering.Order;
+
 namespace Ordering.Repository
 {
-    public class OrderFileRepository
+    public class OrderFileRepository: IOrderRepository
     {
         public OrderFileRepository()
         {
+        }
+
+        public IOrder Create(IOrder order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(IOrder order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOrder Read(IOrderId orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOrder Update(IOrder order)
+        {
+            throw new NotImplementedException();
         }
     }
 }
